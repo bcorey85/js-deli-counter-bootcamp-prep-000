9c020164e66061f43131d2e063fc3ce3dff7a361
brandon corey <bcorey85@gmail.com>
2019-03-30 21:36:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// function nowServing(katzDeliLine) {
-//   if(katzDeliLine.length > 0) {
-//     return (`Currently serving ${katzDeliLine.shift()}.`)
-//   } else {
-//     return("There is nobody waiting to be served!")
-//   }
-// }
-
-
 // function currentLine(katzDeliLine) {
 //   var newLine = []
 // 	if (katzDeliLine.length == 0) {
